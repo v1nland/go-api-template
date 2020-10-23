ccf187160580f83e3907fc13549b57fc8be2dbaa
Martín Saavedra Núñez <martin.saavedra@mail.udp.cl>
2020-10-23 13:13:14 -0300

    Nuevos packages, .env de ejemplo y cambio en main..

--- main.go
+++ main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
 	"go-api-template/Config"
 	"go-api-template/Models"
 	"go-api-template/Routers"
+	"os"
 )
 
 var err error
 
 func main() {
-	Config.DB, err = gorm.Open("mysql", "DBUSERNAME:DBPASSWORD@tcp(127.0.0.1:3306)/DBNAME?charset=utf8&parseTime=True&loc=Local")
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("status error: ", err)
+	}
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	password := os.Getenv("DB_PASS")
+	shouldAutomigrate := os.Getenv("SHOULD_AUTOMIGRATE")
+
+	Config.DB, err = gorm.Open("postgres", "host="+host+" port="+port+" user="+user+" dbname="+dbname+" password="+password)
 	if err != nil {
 		fmt.Println("statuse: ", err)
 	}
 	defer Config.DB.Close()
 
-	Config.DB.AutoMigrate(&Models.Usuario{})
+	if shouldAutomigrate == "1" {
+		Config.DB.AutoMigrate(&Models.Usuario{})
+	}
 
 	r := Routers.SetupRouter()
 
